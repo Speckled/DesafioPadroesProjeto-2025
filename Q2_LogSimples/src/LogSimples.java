8979a3e6ff62a87b9d4e8a6eff78e48aabf5cbbf
Rafael Blanco Brum <rafaelbbrum90@gmail.com>
2025-08-20 19:24:45 -0300

    Update LogSimples.java

--- Q2_LogSimples/src/LogSimples.java
+++ Q2_LogSimples/src/LogSimples.java
@@ -4,15 +4,25 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class LogSimples implements Iterable<String>{
-    private List<String> mensagens;
+   
+    private List<String> mensagens; 
+    
+    private static LogSimples instance;
 
-    public LogSimples(){
+    private LogSimples(){
         mensagens = new LinkedList<>();
     }
-
-    public void log(String m){
+    
+    public static void log(String m){
         String logM = LocalDate.now().toString() + " : " + m;
-        mensagens.add(logM);
+        instance.mensagens.add(logM);
+    }
+
+    public static LogSimples getInstance(){
+        if(instance == null){
+            instance = new LogSimples();
+        }
+        return instance;
     }
 
     @Override
